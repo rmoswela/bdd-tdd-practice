09402e2ae3125ca3294af3d87857b2260b5176f5
Reuben <rmoswela@gmail.com>
2023-02-28 19:24:54 +0200

    initial commit for tdd practice course

---
+++ tdd-udemy-course/tddPractice/project-one/NaiveCanonicalizer.cs
@@ -0,0 +1,23 @@
+﻿using System;
+namespace project_one
+{
+   /**
+    * This is a test class used to demostrate why we need to write test
+    */
+   public class NaiveCanonicalizer
+   {
+      public static string GetCanonicalForm(string searchTerm)
+      {
+         if (searchTerm == null)
+            throw new ArgumentException("searchTerm");
+
+         return searchTerm
+            .Split(new[] {' '}, StringSplitOptions.RemoveEmptyEntries)
+            .Select(x => x.ToUpper())
+            .OrderBy(x=>x)
+            .Aggregate("",(x, y) => x + " " + y)
+            .Trim();
+      }
+   }
+}
+
---
+++ tdd-udemy-course/tddPractice/project-one/Program.cs
@@ -0,0 +1,23 @@
+﻿namespace project_one
+{
+   class Program
+   {
+      static void Main(string[] args)
+      {
+         string empty = NaiveCanonicalizer.GetCanonicalForm("");
+         Console.WriteLine(empty=="");
+         empty = NaiveCanonicalizer.GetCanonicalForm(" ");
+         Console.WriteLine(empty=="");
+         empty = NaiveCanonicalizer.GetCanonicalForm("      ");
+         Console.WriteLine(empty=="");
+
+         Console.WriteLine(NaiveCanonicalizer.GetCanonicalForm(" I am      so    Hood DJ Khaled   "));
+         Console.WriteLine(NaiveCanonicalizer.GetCanonicalForm("Hood    I     am   so   DJ Khaled    "));
+
+         Console.WriteLine(NaiveCanonicalizer.GetCanonicalForm("I am so hood DJ Khaled"));
+         Console.WriteLine(NaiveCanonicalizer.GetCanonicalForm("Hood I am so DJ Khaled"));
+         Console.WriteLine(NaiveCanonicalizer.GetCanonicalForm("DJ Khaled Hood I am so"));
+         Console.Read();
+      }
+   }
+}
