1b89e547a97dd6ea2c82f5c2bee57279ac80539b
Reuben <rmoswela@gmail.com>
2023-03-02 20:45:14 +0200

    adds the fibonacci tdd tests

--- tdd-udemy-course/tddPractice/projects-tests/ProjectOneTests.cs
+++ tdd-udemy-course/tddPractice/projects-tests/ProjectOneTests.cs
@@ -13,3 +13,33 @@ public class CalculatorTests
       Assert.That(result, Is.EqualTo(5));
    }
 }
+
+/**
+ * Sequence of fibonacci numbers
+ * 1, 1, 2, 3, 5, 8, 13, 21, 34, 45
+ * or
+ * 0, 1, 1, 2, 3, 5, 8, 13, 21, 34, 45
+ * */
+[TestFixture]
+public class FibonacciTests
+{
+   [TestCase(0, 0)]
+   [TestCase(1, 1)]
+   [TestCase(1, 2)]
+   [TestCase(2, 3)]
+   [TestCase(3, 4)]
+   [TestCase(5, 5)]
+   [TestCase(8, 6)]
+   [Test]
+   public void TestFibonacci(int expected, int index)
+   {
+      //Arrange
+      Fibonacci fibo = new Fibonacci();
+
+      //Act
+      var actual = fibo.GetFibonacci(index);
+
+      //Assert
+      Assert.That(expected, Is.EqualTo(actual));
+   }
+}
