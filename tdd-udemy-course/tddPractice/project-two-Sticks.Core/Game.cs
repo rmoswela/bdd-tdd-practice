c9f069f450a81ac482f44c8bf93c8ec939199ce1
Reuben <rmoswela@gmail.com>
2023-03-13 23:16:21 +0200

    adds game logic to have the correct game state remain if valid number of sticks are picked

--- tdd-udemy-course/tddPractice/project-two-Sticks.Core/Game.cs
+++ tdd-udemy-course/tddPractice/project-two-Sticks.Core/Game.cs
@@ -16,12 +16,30 @@ public class Game
       }
    }
 
-   public void HumanMakesMove(int sticksTaken)
+   /// <summary>
+   /// Copy of constructor
+   /// </summary>
+   /// <param name="turn"></param>
+   /// <param name="numberOfSticks"></param>
+   private Game(Player turn, int numberOfSticks)
+   {
+      NumberOfSticks = numberOfSticks;
+      Turn = turn;
+   }
+
+   public Game HumanMakesMove(int sticksTaken)
    {
       if (sticksTaken < MinToTake || sticksTaken > MaxToTake)
       {
          throw new ArgumentException($"You should take one to three sticks. You took: {sticksTaken}");
       }
+
+      return new Game(Revert(Turn), NumberOfSticks - sticksTaken);
+   }
+
+   private Player Revert(Player p)
+   {
+      return p == Player.Machine ? Player.Human : Player.Machine;
    }
 
 
