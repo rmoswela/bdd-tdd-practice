616993c8fc0f97242837e19d16e829e17d11d22b
Reuben <rmoswela@gmail.com>
2023-03-20 18:18:27 +0200

    adds test case to push one item into the stack

--- tdd-udemy-course/tddPractice/project-three-Kata.Stack.Tests/StackTests.cs
+++ tdd-udemy-course/tddPractice/project-three-Kata.Stack.Tests/StackTests.cs
@@ -3,21 +3,36 @@
 public class StackTests
 {
    [Test]
-    public void IsStackEmpty_EmptyStack_ReturnsTrue()
-    {
+   public void IsStackEmpty_EmptyStack_ReturnsTrue()
+   {
       //Arrange
       var myStack = new MyStack<int>();
       //Act
       bool isEmpty = myStack.IsEmpty;
       //Assert
       Assert.That(isEmpty, Is.True);
-    }
+   }
+
+   [Test]
+   public void Count_PushOneItem_ReturnsOne()
+   {
+      var stack = new MyStack<int>();
+      int value = 35;
+
+      stack.Push(value);
+
+      Assert.That(1, Is.EqualTo(stack.Count));
+   }
 }
 
 public class MyStack<T>
 {
-   public bool IsEmpty
+   public bool IsEmpty => Count == 0;
+
+   public int Count { get; private set; }
+
+   public void Push(T value)
    {
-      get { return true; }
+      Count++;
    }
 }
