5c22b4e3ce31b39118e20ed5924d0e0367273fe4
Reuben <rmoswela@gmail.com>
2023-03-20 18:08:51 +0200

    adds stack test project to develop a stack data structure using tdd

---
+++ tdd-udemy-course/tddPractice/project-three-Kata.Stack.Tests/StackTests.cs
@@ -0,0 +1,23 @@
+﻿namespace project_three_Kata.Tests;
+
+public class StackTests
+{
+   [Test]
+    public void IsStackEmpty_EmptyStack_ReturnsTrue()
+    {
+      //Arrange
+      var myStack = new MyStack<int>();
+      //Act
+      bool isEmpty = myStack.IsEmpty;
+      //Assert
+      Assert.That(isEmpty, Is.True);
+    }
+}
+
+public class MyStack<T>
+{
+   public bool IsEmpty
+   {
+      get { return true; }
+   }
+}
