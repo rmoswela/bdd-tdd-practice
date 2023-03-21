b46664c1247bf8c074941e876e9803e7a452afbc
Reuben <rmoswela@gmail.com>
2023-03-21 18:51:06 +0200

    cleans up the solution and adds a new project and class for mystack

---
+++ tdd-udemy-course/tddPractice/project-three-Kata.Stack.Core/MyStack.cs
@@ -0,0 +1,33 @@
+﻿namespace project_three_Kata.Core;
+/// <summary>
+/// A class that emulates behaviour of a Stack
+/// Last In First Out (LIFO)
+/// </summary>
+public class MyStack<T>
+{
+   private List<T> _stackList = new List<T>();
+
+   public bool IsEmpty => Count == 0;
+
+   public int Count => _stackList.Count;
+
+   public void Push(T value)
+   {
+      _stackList.Add(value);
+   }
+
+   public void Pop()
+   {
+      if (IsEmpty)
+      {
+         throw new InvalidOperationException();
+      }
+      _stackList.RemoveAt(Count - 1);
+   }
+
+   public T Peek()
+   {
+      return _stackList[Count - 1];
+   }
+}
+
--- tdd-udemy-course/tddPractice/project-three-Kata.Stack.Tests/StackTests.cs
+++ tdd-udemy-course/tddPractice/project-three-Kata.Stack.Tests/StackTests.cs
@@ -61,31 +61,3 @@ public class StackTests
       Assert.That("Lame", Is.EqualTo(headValue));
    }
 }
-
-public class MyStack<T>
-{
-   private List<T> _stackList = new List<T>();
-   
-   public bool IsEmpty => Count == 0;
-
-   public int Count => _stackList.Count;
-
-   public void Push(T value)
-   {
-      _stackList.Add(value);
-   }
-
-   public void Pop()
-   {
-      if (IsEmpty)
-      {
-         throw new InvalidOperationException();
-      }
-      _stackList.RemoveAt(Count - 1);
-   }
-
-   public T Peek()
-   {
-      return _stackList[Count - 1];
-   }
-}
