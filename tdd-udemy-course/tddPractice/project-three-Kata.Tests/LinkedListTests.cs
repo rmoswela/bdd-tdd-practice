032953c4b5a4d2f467ccb1b34a78d60d91ff235b
Reuben <rmoswela@gmail.com>
2023-03-24 21:31:41 +0200

    adds test method that check if head and tail of list are same after adding first item

--- tdd-udemy-course/tddPractice/project-three-Kata.Tests/LinkedListTests.cs
+++ tdd-udemy-course/tddPractice/project-three-Kata.Tests/LinkedListTests.cs
@@ -5,15 +5,41 @@ namespace project_three_Kata.Tests
    public class LinkedListTests
    {
       [Test]
-      public void CreateNode_SetsValueAndNextIsNull()
+      public void CreateNode_SetsValueAndNextIsNull_ReturnsTrue()
       {
          ListNode<int> node = new ListNode<int>(10);
 
          Assert.That(10, Is.EqualTo(node.Value));
          Assert.IsNull(node.Next);
       }
+
+      [Test]
+      public void AddFirst_CheckIfHeadAndTailAreSame_ReturnsTrue()
+      {
+         MyLinkedList<int> list = new MyLinkedList<int>();
+         list.AddFirst(30);
+
+         Assert.That(30, Is.EqualTo(list.Head.Value));
+         Assert.That(30, Is.EqualTo(list.Tail.Value));
+         Assert.That(list.Head, Is.SameAs(list.Tail));
+      }
    }
 
+   public class MyLinkedList<T>
+   {
+      public ListNode<T> Head { get; set; }
+      public ListNode<T> Tail { get; set; }
+
+      public void AddFirst(T value)
+      {
+         AddFirst(new ListNode<T> (value));
+      }
+
+      private void AddFirst(ListNode<T> node)
+      {
+         Tail = Head = node;
+      }
+   }
 
    public class ListNode<T>
    {
