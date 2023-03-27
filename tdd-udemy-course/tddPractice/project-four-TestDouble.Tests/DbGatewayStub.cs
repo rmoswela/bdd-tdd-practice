4b169bf6dfdd59c8bf88ad29e1d33262c9c04d7e
Reuben <rmoswela@gmail.com>
2023-03-27 21:23:11 +0200

    adds test double stub implementation

---
+++ tdd-udemy-course/tddPractice/project-four-TestDouble.Tests/DbGatewayStub.cs
@@ -0,0 +1,23 @@
+﻿using project_four_TestDouble.Core;
+using System;
+namespace project_four_TestDouble.Tests
+{
+   //Stubs provide pre determined responses for method calls
+   //And will always return the same results regardless of input or how is called
+   //It also cannot fail itself
+  public class DbGatewayStub : IDbGateway
+  {
+      private EmployeeStats _employeeStats;
+
+      public EmployeeStats GetEmployeeStats(int userId)
+      {
+         return _employeeStats;
+      }
+
+      public void SetEmployeeStats(EmployeeStats es)
+      {
+         _employeeStats = es;
+      }
+  }
+}
+
