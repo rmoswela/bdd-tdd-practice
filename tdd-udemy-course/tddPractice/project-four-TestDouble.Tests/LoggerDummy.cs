8da31d50ea7867b8a7f60e5479c80800fbb3e657
Reuben <rmoswela@gmail.com>
2023-03-27 21:19:19 +0200

    adds test double dummy implementation

---
+++ tdd-udemy-course/tddPractice/project-four-TestDouble.Tests/LoggerDummy.cs
@@ -0,0 +1,16 @@
+﻿using project_four_TestDouble.Core;
+using System;
+namespace project_four_TestDouble.Tests
+{
+   //Dummies test double generally are added to methods
+   //which we want to do nothing or return null for ref types or zero for values
+   //basically no logic
+  public class LoggerDummy : ILogger
+  {
+      public void Info(string message)
+      {
+         //Do nothing
+      }
+  }
+}
+
