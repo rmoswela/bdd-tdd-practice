336446b67294b69b4fc668efac81a9dae96c1e4b
Reuben <rmoswela@gmail.com>
2023-03-28 21:34:47 +0200

    adds test double spy implementation

---
+++ tdd-udemy-course/tddPractice/project-four-TestDouble.Tests/DbGatewaySpy.cs
@@ -0,0 +1,24 @@
+﻿using project_four_TestDouble.Core;
+using System;
+namespace project_four_TestDouble.Tests
+{
+   //Spies record information about method calls made
+   //useful for verifying method was called or called with correct parameters
+   public class DbGatewaySpy : IDbGateway
+   {
+      private EmployeeStats _employeeStats;
+      public int EmployeeId { get; private set; }
+
+      public EmployeeStats GetEmployeeStats(int employeeId)
+      {
+         EmployeeId = employeeId;
+         return _employeeStats;
+      }
+
+      public void SetEmployeeStats(EmployeeStats es)
+      {
+         _employeeStats = es;
+      }
+   }
+}
+
