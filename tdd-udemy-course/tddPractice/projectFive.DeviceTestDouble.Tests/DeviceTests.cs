47ce4f733da6defbff0ffe82b3ae7b6619612404
Reuben <rmoswela@gmail.com>
2023-03-31 13:50:20 +0200

    adds test project and initial test for device connection retries

---
+++ tdd-udemy-course/tddPractice/projectFive.DeviceTestDouble.Tests/DeviceTests.cs
@@ -0,0 +1,26 @@
+﻿using NSubstitute;
+using projectFive.DeviceTestDouble.Core;
+
+namespace projectFive.DeviceTestDouble.Tests;
+
+[TestFixture]
+public class DeviceTests
+{
+   [Test]
+   public void Connect_OnFailure_RetryThreeTimes()
+   {
+      // Arrange
+      var protocolProvider = Substitute.For<IProtocol>();
+      var device = new Device(protocolProvider);
+      protocolProvider.Connect(Arg.Any<string>()).Returns(false);
+
+      //protocolProvider.Connect(Arg.Is("COM1")).Returns(true);
+      //protocolProvider.Connect(Arg.Is<string>(x => x.StartsWith("COM1"))).Returns(true);
+
+      // Act
+      device.Connect(string.Empty);
+
+      // Assert
+      protocolProvider.Received(3).Connect(Arg.Any<string>());
+   }
+}
